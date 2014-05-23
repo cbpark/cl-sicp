657e2d9214570927ee311ef15f215f5a7acd0967
cbpark <cbpark@gmail.com>
2014-05-23 21:29:41 +0200

    Fix typo

--- src/chapter3.lisp
+++ src/chapter3.lisp
@@ -3,7 +3,7 @@
   (:use :cl))
 (in-package :cl-sicp.chapter3)
 
-;;; Chapter 2. Modularity, Objects, and State
+;;; Chapter 3. Modularity, Objects, and State
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 3.1 Assignment and Local State
